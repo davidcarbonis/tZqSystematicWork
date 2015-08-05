3a4b6f0e665ba35a6e38364212f697e56b428a2d
Alexander <alexander.david.morton@cern.ch>
2015-08-05 13:20:50 +0100

    Fuller array of histograms - lepton/quark pairs from Z plotting

--- TTZgenLevelAnalyser/src/TTZgenLevelAnalyser.cc
+++ TTZgenLevelAnalyser/src/TTZgenLevelAnalyser.cc
@@ -42,6 +42,7 @@
 #include "FWCore/Framework/interface/EDAnalyzer.h"
 
 #include "TH1.h"
+#include "TLatex.h"
 
 //
 // class declaration
@@ -70,6 +71,16 @@ private:
   TH1D* h_muonMass;
   TH1D* h_tauMass;
   TH1D* h_zMass;
+
+  TH1D* h_electronMassFromZ;
+  TH1D* h_muonMassFromZ;
+  TH1D* h_tauMassFromZ;
+
+  TH1D* h_upMassFromZ;
+  TH1D* h_downMassFromZ;
+  TH1D* h_strangeMassFromZ;
+  TH1D* h_charmMassFromZ;
+  TH1D* h_bottomMassFromZ;
 
       // ----------member data ---------------------------
 };
@@ -119,16 +130,30 @@ void TTZgenLevelAnalyser::analyze(const edm::Event& iEvent, const edm::EventSetu
   
   //  HepMC::GenEvent* Evt = new HepMC::GenEvent();
 
+  std::vector< int > leptonVector;
+  std::vector< int > quarkVector;
+
   for (int i = 0; i < lheEventSrc->hepeup().NUP; ++i) {
     
     int pgId = lheEventSrc->hepeup().IDUP[i];
-    
+    int status = lheEventSrc->hepeup().ISTUP[i];
+
     HepMC::FourVector p(lheEventSrc->hepeup().PUP[i][0], lheEventSrc->hepeup().PUP[i][1], lheEventSrc->hepeup().PUP[i][2], lheEventSrc->hepeup().PUP[i][3]);
 
     //lheEventSrc->hepeup().PUP[i][4]; generated mass
 
-    //    std::cout << "i: " << i << std::endl;
-    
+    // Look for quark antiquark pair in final state
+    if ( fabs (pgId) <= 5 && status == 1) {
+      quarkVector.push_back( i );
+    }
+
+    // Look for lepton antilepton pair in final state
+    if ( (fabs (pgId) == 11 || fabs (pgId) == 13 || fabs (pgId) == 15) && status == 1 ) {
+      leptonVector.push_back( i );
+    } 
+        
+    // General plot distributions
+
     if ( fabs(pgId) == 6 ) {
       //      std::cout << "Status: " << lheEventSrc->hepeup().ISTUP[i] << "; pgId: Top." << std::endl;
       h_topMass->Fill(p.m());
@@ -136,21 +161,21 @@ void TTZgenLevelAnalyser::analyze(const edm::Event& iEvent, const edm::EventSetu
 
     else if ( fabs(pgId) == 11 ) {
       //      std::cout << "Status: " << lheEventSrc->hepeup().ISTUP[i] << "; pgId: Electron." << std::endl;
-      h_electronMass->Fill(p.m());
+      h_electronMass->Fill(p.m()*1000.0);
     }
 
     else if ( fabs(pgId) == 13 ) {
       //      std::cout << "Status: " << lheEventSrc->hepeup().ISTUP[i] << "; pgId: Muon." << std::endl;
-      h_muonMass->Fill(p.m());
+      h_muonMass->Fill(p.m()*1000.0);
     }
 
     else if ( fabs(pgId) == 15 ) {
       //      std::cout << "Status: " << lheEventSrc->hepeup().ISTUP[i] << "; pgId: Tauon." << std::endl;
-      h_tauMass->Fill(p.m());
+      h_tauMass->Fill(p.m()*1000.0);
     }
 
     else if ( fabs(pgId) == 22 ) {
-      //     std::cout << "Status: " << lheEventSrc->hepeup().ISTUP[i] << "; pgId: Gamma." << std::endl;
+      //      std::cout << "Status: " << lheEventSrc->hepeup().ISTUP[i] << "; pgId: Gamma." << std::endl;
     }
 
     else if ( fabs(pgId) == 23 ) {
@@ -158,14 +183,91 @@ void TTZgenLevelAnalyser::analyze(const edm::Event& iEvent, const edm::EventSetu
       h_zMass->Fill(p.m());
     }
 
-    else {
-      //      std::cout << "Status: " << lheEventSrc->hepeup().ISTUP[i] << "; pgId: " << pgId << std::endl;
-    }
+    //    std::cout << "Status: " << lheEventSrc->hepeup().ISTUP[i] << "; pgId: " << pgId << std::endl;
 
     //    std::cout << "Invariant Mass: " << p.m() << std::endl;
     
   }
 
+  // Check if two leptons are particle-antiparticle pair and from Z/gamma
+  if ( leptonVector.size() == 2 ){
+
+
+    int pgId1 = lheEventSrc->hepeup().IDUP[ leptonVector[0] ];
+    int pgId2 = lheEventSrc->hepeup().IDUP[ leptonVector[1] ];
+
+    //    std::cout << "pgId1/pgId2: " << pgId1 << "/" << pgId2 << std::endl;
+
+    if ( fabs(pgId1) == fabs(pgId2) ) {
+
+      HepMC::FourVector p1(lheEventSrc->hepeup().PUP[ leptonVector[0] ][0], lheEventSrc->hepeup().PUP[ leptonVector[0] ][1], lheEventSrc->hepeup().PUP[ leptonVector[0] ][2], lheEventSrc->hepeup().PUP[ leptonVector[0] ][3]);
+      HepMC::FourVector p2(lheEventSrc->hepeup().PUP[ leptonVector[1] ][0], lheEventSrc->hepeup().PUP[ leptonVector[1] ][1], lheEventSrc->hepeup().PUP[ leptonVector[1] ][2], lheEventSrc->hepeup().PUP[ leptonVector[1] ][3]);
+
+      // electrons
+      if ( fabs(pgId1) == 11 ) {
+	h_electronMassFromZ->Fill( p1.m()*1000.0 );
+	h_electronMassFromZ->Fill( p2.m()*1000.0 );
+      }
+
+      // muons
+      if ( fabs(pgId1) == 13 ) {
+	h_muonMassFromZ->Fill( p1.m()*1000.0 );
+	h_muonMassFromZ->Fill( p2.m()*1000.0 );
+      }
+
+      // taus
+      if ( fabs(pgId1) == 15 ) {
+	h_tauMassFromZ->Fill( p1.m()*1000.0 );
+	h_tauMassFromZ->Fill( p2.m()*1000.0 );
+      }
+    }
+  }
+
+
+  // Check if two quarks are particle-antiparticle pair and from Z/gamma
+  if ( quarkVector.size() == 2 ){
+
+    int pgId1 = lheEventSrc->hepeup().IDUP[ quarkVector[0] ];
+    int pgId2 = lheEventSrc->hepeup().IDUP[ quarkVector[1] ];
+
+    //   std::cout << "pgId1/pgId2: " << pgId1 << "/" << pgId2 << std::endl;
+
+    if ( fabs(pgId1) == fabs(pgId2) ) {
+      
+      HepMC::FourVector p1(lheEventSrc->hepeup().PUP[ quarkVector[0] ][0], lheEventSrc->hepeup().PUP[ quarkVector[0] ][1], lheEventSrc->hepeup().PUP[ quarkVector[0] ][2], lheEventSrc->hepeup().PUP[ quarkVector[0] ][3]);
+      HepMC::FourVector p2(lheEventSrc->hepeup().PUP[ quarkVector[1] ][0], lheEventSrc->hepeup().PUP[ quarkVector[1] ][1], lheEventSrc->hepeup().PUP[ quarkVector[1] ][2], lheEventSrc->hepeup().PUP[ quarkVector[1] ][3]);
+      
+      // down quarks
+      if ( fabs(pgId1) == 1 ) { 
+	h_downMassFromZ->Fill( p1.m()*1000.0 );
+	h_downMassFromZ->Fill( p2.m()*1000.0 );
+      }
+
+      // up quarks
+      if ( fabs(pgId1) == 2 ) {
+	h_upMassFromZ->Fill( p1.m()*1000.0 );
+	h_upMassFromZ->Fill( p2.m()*1000.0 );
+      }
+
+      // strange quarks
+      if ( fabs(pgId1) == 3 ) {
+	h_strangeMassFromZ->Fill( p1.m()*1000.0 );
+	h_strangeMassFromZ->Fill( p2.m()*1000.0 );
+      }
+
+      // strange quarks
+      if ( fabs(pgId1) == 4 ) {
+	h_charmMassFromZ->Fill( p1.m()*1000.0 );
+	h_charmMassFromZ->Fill( p2.m()*1000.0 );
+      }
+      
+      // bottom quarks
+      if ( fabs(pgId1) == 5 ) {
+	h_bottomMassFromZ->Fill( p1.m()*1000.0 );
+	h_bottomMassFromZ->Fill( p2.m()*1000.0 );
+      }
+    }
+  }
 }
 
 
@@ -174,11 +276,21 @@ void TTZgenLevelAnalyser::beginJob()
 {
   edm::Service<TFileService> fs;
 
-  h_topMass = fs->make<TH1D>( "h_topMass", "e+/e- inv. mass", 100, 0.0, 10.0 );
-  h_electronMass = fs->make<TH1D>( "h_electronMass", "e+/e- inv. mass", 100, 0.0, 10.0 );
-  h_muonMass = fs->make<TH1D>( "h_muonMass", "mu+/mu- inv. mass", 100, 60.0, 120.0);
-  h_tauMass = fs->make<TH1D>( "h_tauMass", "tau+/tau- inv. mass", 100, 1400.0, 2400.0);
-  h_zMass = fs->make<TH1D>( "h_zMass", "Z inv. mass", 100, 1400.0, 2400.0);
+  h_topMass = fs->make<TH1D>( "h_topMass", "top inv. mass; GeV/c^{2}", 1000, 1.0, 0.0 );
+  h_electronMass = fs->make<TH1D>( "h_electronMass", "e+/e- inv. mass; MeV/c^{2}", 1000, -10.0, 10.0 );
+  h_muonMass = fs->make<TH1D>( "h_muonMass", "mu+/mu- inv. mass; MeV/c^{2}", 1000, -10.0, 10.0);
+  h_tauMass = fs->make<TH1D>( "h_tauMass", "tau+/tau- inv. mass; MeV/c^{2}", 1000, 1770.0, 1790.0);
+  h_zMass = fs->make<TH1D>( "h_zMass", "Z inv. mass; GeV/c^{2}", 1000, 50.0, 130.0);
+
+  h_electronMassFromZ = fs->make<TH1D>( "h_electronMassFromZ", "e+/e- from Z decay inv. mass; MeV/c^{2}", 1000, -10.0, 10.0 );
+  h_muonMassFromZ = fs->make<TH1D>( "h_muonMassFromZ", "mu+/mu- from Z decay inv. mass; MeV/c^{2}", 1000, -10.0, 10.0);
+  h_tauMassFromZ = fs->make<TH1D>( "h_tauMassFromZ", "tau+/tau- from Z decay inv. mass; MeV/c^{2}", 1000, 1770.0, 1790.0);
+
+  h_upMassFromZ = fs->make<TH1D>( "h_upMassFromZ", "u/#bar{u} from Z decay inv. mass;", 1000, -20.0, 20.0 );
+  h_downMassFromZ = fs->make<TH1D>( "h_downMassFromZ", "d/#bar{d} from Z decay inv. mass;", 1000, -20.0, 20.0);
+  h_strangeMassFromZ = fs->make<TH1D>( "h_strangeMassFromZ", "s/#bar{s} from Z decay inv. mass;", 1000, -20.0, 20.0);
+  h_charmMassFromZ = fs->make<TH1D>( "h_charmMassFromZ", "c/#bar{c} from Z decay inv. mass;", 1000, -20.0, 20.0 );
+  h_bottomMassFromZ = fs->make<TH1D>( "h_bottomMassFromZ", "b/#bar{b} from Z decay inv. mass;", 1000, -20.0, 20.0);
 
 }
 
--- TTZgenLevelAnalyser/test/ttzgenlevelanalyser_cfg.py
+++ TTZgenLevelAnalyser/test/ttzgenlevelanalyser_cfg.py
@@ -7,26 +7,26 @@ process.load("FWCore.MessageService.MessageLogger_cfi")
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.MessageLogger.cerr.INFO = cms.untracked.PSet(limit = cms.untracked.int32(0))
-process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(10000)
+process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(100000)
 
 process.load( "SimGeneral.HepPDTESSource.pythiapdt_cfi" )
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
         ## Scale up files
-        #'file:/nfs/data/eepgadm/tZq/MC/2012/TTZJets_8TeV_scaleup/GEN/genFile0.root',
-        #'file:/nfs/data/eepgadm/tZq/MC/2012/TTZJets_8TeV_scaleup/GEN/genFile1.root',
-        #'file:/nfs/data/eepgadm/tZq/MC/2012/TTZJets_8TeV_scaleup/GEN/genFile2.root',
+        'file:/nfs/data/eepgadm/tZq/MC/2012/TTZJets_8TeV_scaleup/GEN/genFile0.root',
+        'file:/nfs/data/eepgadm/tZq/MC/2012/TTZJets_8TeV_scaleup/GEN/genFile1.root',
+        'file:/nfs/data/eepgadm/tZq/MC/2012/TTZJets_8TeV_scaleup/GEN/genFile2.root',
 
         ## Scale down files
-        'file:/nfs/data/eepgadm/tZq/MC/2012/TTZJets_8TeV_scaledown/GEN/genFile0.root',
-        'file:/nfs/data/eepgadm/tZq/MC/2012/TTZJets_8TeV_scaledown/GEN/genFile1.root',
-        'file:/nfs/data/eepgadm/tZq/MC/2012/TTZJets_8TeV_scaledown/GEN/genFile2.root',
+        #'file:/nfs/data/eepgadm/tZq/MC/2012/TTZJets_8TeV_scaledown/GEN/genFile0.root',
+        #'file:/nfs/data/eepgadm/tZq/MC/2012/TTZJets_8TeV_scaledown/GEN/genFile1.root',
+        #'file:/nfs/data/eepgadm/tZq/MC/2012/TTZJets_8TeV_scaledown/GEN/genFile2.root',
     )
 )
 
 process.TFileService = cms.Service("TFileService",
-        fileName = cms.string("ScaleDownOutput.root")
+        fileName = cms.string("ScaleUpOutput.root")
 )
 
 
